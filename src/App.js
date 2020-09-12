b07eb62fa46fb8b6f09367ea7faf422d3283ee32
13circle <13circle97@gmail.com>
2020-09-13 02:38:37 +0900

    Tutorial Ch.3: JSX
    Link: https://velopert.com/3626
    
    - [Component]: Apply class-based component
    - [JSX]: Apply Fragment & apply js with conditional rendering
    - [App.css]: Add class name from App.css to className prop
    - [Comment]: Test both in/outside of the block

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function boo(option, isSudo) {
+  let isFrom = option == "--from";
+  if (isFrom && isSudo) {
+    return "Aaahhhhh~!!!!";
+  }
+  return "Nothing happened. Exit status 1.";
+}
+
+class App extends Component {
+  render() {
+    const sampleName = "React";
+    const style = {
+      backgroundColor: "#000000",
+      color: "white",
+      margin: "2vw",
+      padding: "1vw",
+      width: "fit-content",
+    };
+    const isRender = true;
+    return (
+      <Fragment>
+        {/* This is a sample comment */}
+        <div style={style}>
+          <label className="test-className">Simple Text: </label>
+          Hello, {sampleName}!
+        </div>
+        <div style={style}>
+          <label>Input Text: </label>
+          {isRender && <input type="text" placeholder="Sample Input 1" />}
+          {(() => {
+            // This is a sample comment
+            if (!isRender) {
+              return <input type="text" placeholder="Sample Input 2" />;
+            }
+          })()}
+        </div>
+        <div style={style}>
+          <label>Programmar Meme Quote: </label>
+          <br />
+          <code>
+            boo --from ghost <br />
+            {boo("--form", false)} <br />
+            sudo boo --from ghost <br />
+            {boo("--from", true)} <br />
+          </code>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
