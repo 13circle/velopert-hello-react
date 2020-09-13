0173f3d5c1d5374b65e129b96a4ef878c8a9015f
13circle <13circle97@gmail.com>
2020-09-13 17:57:07 +0900

    Updates

---
+++ precache-manifest.39430eed62ed9d460d326139064f3b40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "addb712c43a6e9355552b315bf3b0e90",
+    "url": "/velopert-hello-react/index.html"
+  },
+  {
+    "revision": "2503ce3e072af3154a55",
+    "url": "/velopert-hello-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "68ead8149550b5f81082",
+    "url": "/velopert-hello-react/static/js/2.ab6c9de5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/velopert-hello-react/static/js/2.ab6c9de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2503ce3e072af3154a55",
+    "url": "/velopert-hello-react/static/js/main.b7340d45.chunk.js"
+  },
+  {
+    "revision": "b3f15285922248b319b4",
+    "url": "/velopert-hello-react/static/js/runtime-main.757cd3c5.js"
+  }
+]);
